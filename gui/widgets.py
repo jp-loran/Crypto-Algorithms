c8a228e913d0b7de304256d1cb7cebd8f3fac28b
AgustinOscar <reyesglez_oscar@comunidad.unam.mx>
2022-12-07 15:57:34 -0600

    Avances de gui

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,4 +1,33 @@
-from tkinter import Frame, Label
+from tkinter import LEFT, Button, Frame, Label
+
+
+'''BUTTONS'''
+
+class ButtonStart(Button):
+    def __init__(self, master, text, event, image, posx, posy):
+        super().__init__(
+            master,
+            text = text,
+            command = event,
+            cursor = 'hand2',
+            width = 100,
+            relief = 'groove',
+            bd = 1,
+            bg='steel blue',
+            fg = 'white',
+            font = ('verdana', 12, 'bold'),
+            activeforeground = 'white',
+            activebackground = 'gray50',
+            image = image, 
+            compound = LEFT
+        )
+        self.place(x=posx, y=posy)
+        self.cambiar_boton(self, 'gray90', 'white')
+    
+    #Función para cambiar las propiedades de los botones.
+    def cambiar_boton(self, boton, colorletra, colorLetra2):
+        boton.bind('<Enter>', func = lambda e: boton.config(foreground=colorletra))
+        boton.bind('<Leave>', func = lambda e: boton.config(foreground=colorLetra2))
 
 
 
@@ -10,7 +39,7 @@ class FrameStudentData(Frame):
         super().__init__(
             master,
             width = 699,
-            height = 549,
+            height = 569,
             relief = 'groove',
             bd = 1,
             bg='gray90',
@@ -25,7 +54,19 @@ class LabelStudentData(Label):
             master = master,
             text = text,
             font = ('Verdana', fontsize, 'bold'),
-            fg = 'red3',
+            fg = 'black',
             bg = 'gray90'
         )
         self.place(x=posx, y=posy)
+
+#Label para logo de Santander del menú principal.
+class LabelLogo(Label):
+    def __init__(self, padre, imagen):
+        super().__init__(
+                padre,
+                image = imagen,
+                relief = 'groove',
+                bd = 0,
+                bg = 'gray90'
+            )
+        self.place(x=285, y=10)
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Crypto.Random import get_random_bytes
 import crypto_algorithms as crypto
 import performance as pf
 import json
-from tkinter import Tk
-from gui.widgets import FrameStudentData, LabelStudentData
+from tkinter import PhotoImage, Tk
+from gui.widgets import ButtonStart, FrameStudentData, LabelLogo, LabelStudentData
+from PIL import Image, ImageTk
 
 #TEST VECTOR
 key = get_random_bytes(32) #256 bits  
@@ -15,10 +16,21 @@ print(result)
 #Se crea ventana principal.
 window = Tk()
 window.title('Criptografía')
-window.geometry('700x550+300+100')
+window.geometry('700x570+300+100')
 window.resizable(False, False)
 frame_sd = FrameStudentData(window)
-label_sd1 = LabelStudentData(frame_sd, 'UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO', 16, 55, 10)
-label_sd2 = LabelStudentData(frame_sd, 'FACULTAD DE INGENIERÍA', 16, 190, 40)
-label_sd3 = LabelStudentData(frame_sd, 'CRIPTOGRAFÍA', 16, 255, 70)
+imagen = ImageTk.PhotoImage(Image.open('./gui/images/fi.png'))
+LabelLogo(frame_sd, imagen)
+LabelStudentData(frame_sd, 'UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO', 14, 89, 175)
+LabelStudentData(frame_sd, 'FACULTAD DE INGENIERÍA', 14, 206, 205)
+LabelStudentData(frame_sd, 'CRIPTOGRAFÍA', 14, 265, 235)
+LabelStudentData(frame_sd, 'PROYECTO FINAL', 14, 255, 265)
+LabelStudentData(frame_sd, 'INTEGRANTES', 14, 270, 315)
+LabelStudentData(frame_sd, 'REYES GONZÁLEZ AGUSTÍN ÓSCAR', 12, 185, 355)
+LabelStudentData(frame_sd, 'REYES GONZÁLEZ AGUSTÍN ÓSCAR', 12, 185, 390)
+LabelStudentData(frame_sd, 'REYES GONZÁLEZ AGUSTÍN ÓSCAR', 12, 183, 425)
+photo = PhotoImage(file = './gui/images/key_start.png')
+photoimage = photo.subsample(3, 3)
+ButtonStart(frame_sd, 'Iniciar', None, photoimage, 285, 500)
+
 window.mainloop()
